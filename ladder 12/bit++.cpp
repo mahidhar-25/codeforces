f2c54b335f8cb6f723a720e605a1fca20ad8cf74
mahidhar-25 <mahidhar100008@gmail.com>
2022-08-28 10:29:32 +0530

    chnages

--- ladder 12/bit++.cpp
+++ ladder 12/bit++.cpp
@@ -1,35 +1,41 @@
+//#include <bits/stdc++.h>
 #include<iostream>
 #include<string>
 #include<limits>
 #include<math.h>
 #include<iomanip>
 #include<vector>
-// #pragma GCC optimize("Ofast")
+#include<set>
+//#pragma GCC optimize(`Ofast`)
 using namespace std;
 #define ar array
 #define ll long long int
 #define vi vector<int>
-#define vvi vector<vector<int>>
 #define all(x) x.begin(),x.end()
 const int MAX_N = 1e5 + 1;
 const ll MOD = 1e9 + 7;
 const ll INF = 1e9;
-
-
+ 
+ 
 void solve(){
-    int n,sumx=0,sumy=0,sumz=0,sum=0;
-    cin>>n;
-    vector<vi>a(n , vi(3));
-    for(int i=0;i<n;i++){
-        cin>>a[i][0]>>a[i][1]>>a[i][2];
-        sumx += a[i][0]; sumy+=a[i][1]; sumz+=a[i][2]; sum = sumx + sumy + sumz;
+ 
+ int n,x=0;cin>>n;
+ set<char>s;
+ char c;
+ for(int i=0;i<n;i++){
+    string p; cin>>p;
+    for(int j=0;j<p.size();j++){
+        if(p[j]!='X'){
+         c = p[j];
+        }
     }
-    if(sum==0 && sumx==0 && sumy==0 && sumz==0 ){
-        cout<<"YES"<<"\n";
-    }else{
-        cout<<"NO"<<"\n";
-    }
+    if(c=='+')x++;
+    if(c=='-')x--;
+ }
+ cout<<x<<endl;
 }
+ 
+ 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
--- ladder 12/chips.cpp
+++ ladder 12/chips.cpp
@@ -1,35 +1,27 @@
+//#include <bits/stdc++.h>
 #include<iostream>
 #include<string>
 #include<limits>
 #include<math.h>
 #include<iomanip>
 #include<vector>
-// #pragma GCC optimize("Ofast")
+//#pragma GCC optimize(`Ofast`)
 using namespace std;
 #define ar array
 #define ll long long int
 #define vi vector<int>
-#define vvi vector<vector<int>>
 #define all(x) x.begin(),x.end()
 const int MAX_N = 1e5 + 1;
 const ll MOD = 1e9 + 7;
 const ll INF = 1e9;
-
-
+ 
+ 
 void solve(){
-    int n,sumx=0,sumy=0,sumz=0,sum=0;
-    cin>>n;
-    vector<vi>a(n , vi(3));
-    for(int i=0;i<n;i++){
-        cin>>a[i][0]>>a[i][1]>>a[i][2];
-        sumx += a[i][0]; sumy+=a[i][1]; sumz+=a[i][2]; sum = sumx + sumy + sumz;
-    }
-    if(sum==0 && sumx==0 && sumy==0 && sumz==0 ){
-        cout<<"YES"<<"\n";
-    }else{
-        cout<<"NO"<<"\n";
-    }
+ 
+ 
 }
+ 
+ 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
